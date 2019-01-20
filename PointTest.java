ffd5f83789deaf558561f3a6115f34cbaf648b89
anshu247 <46483372+anshu247@users.noreply.github.com>
2019-01-20 19:05:08 +0530

    Update PointTest.java

--- PointTest.java
+++ PointTest.java
@@ -1,26 +1,19 @@
-
 import java.util.*;
 class PointTest
 {
-	public static void main (String []args)
-	{
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the value of x1: ");
-		float x1 = scan.nextFloat();
-		System.out.println("Enter the value of y1: ");
-		float y1 = scan.nextFloat();
-		System.out.println("Enter the value of x2: ");
-		float x2 = scan.nextFloat();
-		System.out.println("Enter the value of y2: ");
-		float y2 = scan.nextFloat();
-		
-		Point p1 = new Point();
-		Point p2 = new Point();
-		p1.setValuex(x1);
-		p1.setValuey(y1);
-		p2.setValuex(x2);
-		p2.setValuey(y2);
-		float dist = p1.calDis(p2);
-		System.out.println("The distance between the two points is: "+String.format("%7.2f",dist));
-	}
-}
+public static void main(String []args)
+{
+float x1,x2,y1,y2;
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter values of x1,x2,y1,y2 respectively :  ");
+x1=scan.nextFloat();
+x2=scan.nextFloat();
+y1=scan.nextFloat();
+y2=scan.nextFloat();
+
+Point p1=new Point(x1,y1);
+Point p2=new Point(x2,y2);
+
+float dist=p1.calDistance(p2);
+System.out.println("The distance between point 1 :  " +p1.getValuex()+""+p2.getValuey()) ;
+}}
